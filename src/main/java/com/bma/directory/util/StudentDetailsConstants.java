42445a26f161b81d1d643ce58ff04322b2f0fb67
Bikash Yadav <bikashjadav333@gmail.com>
2024-01-22 00:07:12 +0530

    constant fields commit

---
+++ src/main/java/com/bma/directory/util/StudentDetailsConstants.java
@@ -0,0 +1,14 @@
+package com.bma.directory.util;
+
+public class StudentDetailsConstants {
+	public static int GRADE_ONE = 500;
+	public static int GRADE_TWO = 550;
+	public static int GRADE_THREE = 600;
+	public static int GRADE_FOUR = 700;
+	public static int GRADE_FIVE = 800;
+	public static int GRADE_SIX = 1000;
+	public static int GRADE_SEVEN = 1200;
+	public static int GRADE_EIGHT = 1500;
+	public static int GRADE_NINE= 2000;
+	public static int GRADE_TEN = 2500;
+}
